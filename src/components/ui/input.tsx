05aa8f8702971aaffccdc3deff64ee6a78711811
noxzym <orchitiadi@gmail.com>
2024-10-25 20:24:55 +0700

    chore(components/ui): add input component

---
+++ src/components/ui/input.tsx
@@ -0,0 +1,61 @@
+"use client";
+
+import * as React from "react";
+import { IoMdEye, IoMdEyeOff } from "react-icons/io";
+import { cn } from "@/lib/utils";
+
+export interface InputProps extends React.InputHTMLAttributes<HTMLInputElement> {
+    icon?: React.ReactElement;
+}
+
+const Input = React.forwardRef<HTMLInputElement, InputProps>(
+    ({ className, type, icon, ...props }, ref) => {
+        const [showPassword, setShowPassword] = React.useState(false);
+        const Icon = icon;
+
+        const togglePasswordVisibility = () => setShowPassword(!showPassword);
+
+        return (
+            <div className="group relative">
+                {Icon && (
+                    <div className="absolute left-2.5 top-1/2 z-50 -translate-y-1/2 transform">
+                        {icon}
+                    </div>
+                )}
+                <input
+                    ref={ref}
+                    type={type === "password" && showPassword ? "text" : type}
+                    className={cn(
+                        "flex h-10 w-full rounded-md border border-input bg-background px-3 py-2 text-sm ring-offset-background file:border-0 file:bg-transparent file:text-sm file:font-medium file:text-foreground placeholder:text-muted-foreground focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:cursor-not-allowed disabled:opacity-50",
+                        Icon ? "pl-8" : "",
+                        className
+                    )}
+                    {...props}
+                />
+                {type === "password" && (
+                    <div
+                        className={cn(
+                            "absolute right-2.5 top-1/2 z-50 -translate-y-1/2 transform cursor-pointer group-hover:block",
+                            showPassword ? "" : "hidden"
+                        )}
+                    >
+                        {showPassword ? (
+                            <IoMdEyeOff
+                                onClick={togglePasswordVisibility}
+                                className="text-muted-foreground"
+                            />
+                        ) : (
+                            <IoMdEye
+                                onClick={togglePasswordVisibility}
+                                className="text-muted-foreground"
+                            />
+                        )}
+                    </div>
+                )}
+            </div>
+        );
+    }
+);
+Input.displayName = "Input";
+
+export { Input };
