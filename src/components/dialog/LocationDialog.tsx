26195e5e711cf0aa24abe4311a76670a8d3d9d73
noxzym <orchitiadi@gmail.com>
2024-11-21 17:34:21 +0700

    chore(LocationDialog): integrated location with database

--- src/components/dialog/LocationDialog.tsx
+++ src/components/dialog/LocationDialog.tsx
@@ -1,8 +1,10 @@
 import { useEffect, useState } from "react";
-import { ReadonlyURLSearchParams, usePathname, useRouter, useSearchParams } from "next/navigation";
+import { usePathname, useRouter } from "next/navigation";
 import { IoSearch } from "react-icons/io5";
 import { MdLocationOn } from "react-icons/md";
 import { cn } from "@/lib/utils";
+import { useDebounce } from "@/hooks/use-debounce";
+import { useWilayahQuery } from "@/hooks/use-wilayahQuery";
 import { useMediaQuery } from "@/hooks/useMediaQuery";
 import { Button } from "../ui/button";
 import { Dialog, DialogContent, DialogHeader, DialogTitle, DialogTrigger } from "../ui/dialog";
@@ -16,6 +18,7 @@ import {
     DrawerTrigger
 } from "../ui/drawer";
 import { Input } from "../ui/input";
+import { Separator } from "../ui/separator";
 
 interface prop {
     searchParams: string;
@@ -23,31 +26,42 @@ interface prop {
 }
 
 export default function LocationDialog({ searchParams, className }: prop) {
+    const [open, setOpen] = useState(false);
+    const [location, setLocation] = useState<string>();
+    const [inputValue, setInputValue] = useState<string>();
+
+    const inputDebouncedValue = useDebounce(inputValue);
+    const isDesktop = useMediaQuery("(min-width: 768px)");
     const router = useRouter();
     const pathname = usePathname();
 
-    const [open, setOpen] = useState(false);
-    const [location, setLocation] = useState<string>();
-    const isDesktop = useMediaQuery("(min-width: 768px)");
+    const { data } = useWilayahQuery({
+        inputDebouncedValue: open ? inputDebouncedValue?.toUpperCase() : location
+    });
 
     useEffect(() => {
-        if (!location) return;
-
         const params = new URLSearchParams(searchParams);
-        params.set("location", location);
+        const locationQueryId = params.get("location");
 
-        router.push(`${pathname}?${params.toString()}`);
-    }, [location, pathname, router, searchParams]);
+        if (locationQueryId && !location) {
+            return setLocation(locationQueryId);
+        }
+
+        const loc = data?.find(({ id }) => id === locationQueryId);
+        if (!loc) return;
+
+        setLocation(loc.nama.toLowerCase().replace("kab.", "kabupaten"));
+    }, [searchParams, data, location]);
 
     const isSearchPage = pathname === "/search";
-    const title = "Pilih Lokasi Sewa";
-    const content = (
+    const title = "Pilih Kota Tujuan";
+    const input = (
         <Input
             icon={<IoSearch />}
             type="text"
             placeholder="Bali"
             className="text-lg text-foreground"
-            onChange={e => setLocation(e.target.value)}
+            onChange={e => setInputValue(e.target.value)}
         />
     );
 
@@ -55,23 +69,58 @@ export default function LocationDialog({ searchParams, className }: prop) {
         return (
             <Button
                 variant={isSearchPage ? "ghost" : "outline"}
-                className={cn("justify-start font-semibold", className)}
+                className={cn("justify-start font-semibold capitalize", className)}
             >
                 {!isSearchPage && <MdLocationOn />}
-                {location ?? "Jakarta"}
+                {location?.toLowerCase().replace("kab.", "kabupaten") ?? "Jakarta"}
             </Button>
         );
+    }
+
+    function handleSelect(id: string) {
+        const selectedLocation = data?.find(({ id: locationId }) => locationId === id)?.nama;
+        setLocation(selectedLocation);
+
+        const params = new URLSearchParams(searchParams);
+        params.set("location", id);
+        router.push(`${pathname}?${params.toString()}`);
+
+        setOpen(false);
     }
 
     if (isDesktop) {
         return (
             <Dialog open={open} onOpenChange={setOpen}>
                 <DialogTrigger asChild>{triggerButton()}</DialogTrigger>
-                <DialogContent className="py-10">
+                <DialogContent className="flex h-[400px] flex-col py-10">
                     <DialogHeader>
                         <DialogTitle className="text-2xl">{title}</DialogTitle>
+                        {input}
                     </DialogHeader>
-                    {content}
+                    <Separator />
+                    <div className="flex flex-col overflow-y-auto">
+                        {data?.map(({ id, nama, provinsi }) => (
+                            <Button
+                                key={id}
+                                variant="ghost"
+                                className="cursor-pointer justify-start gap-4 py-8 [&_svg]:size-8"
+                                onClick={() => handleSelect(id)}
+                                asChild
+                            >
+                                <div>
+                                    <MdLocationOn />
+                                    <div className="flex flex-col">
+                                        <div className="text-lg font-bold capitalize">
+                                            {nama.toLowerCase().replace("kab.", "kabupaten")}
+                                        </div>
+                                        <div className="capitalize text-gray-500">
+                                            {provinsi.nama.toLowerCase()}, Indonesia
+                                        </div>
+                                    </div>
+                                </div>
+                            </Button>
+                        ))}
+                    </div>
                 </DialogContent>
             </Dialog>
         );
@@ -80,11 +129,35 @@ export default function LocationDialog({ searchParams, className }: prop) {
     return (
         <Drawer open={open} onOpenChange={setOpen}>
             <DrawerTrigger asChild>{triggerButton()}</DrawerTrigger>
-            <DrawerContent>
+            <DrawerContent className="flex h-full max-h-[75vh] flex-col">
                 <DrawerHeader className="text-left">
                     <DrawerTitle className="text-2xl">{title}</DrawerTitle>
+                    {input}
                 </DrawerHeader>
-                <div className="min-h-60 p-4">{content}</div>
+                <Separator />
+                <div className="flex flex-col overflow-y-auto">
+                    {data?.map(({ id, nama, provinsi }) => (
+                        <Button
+                            key={id}
+                            variant="ghost"
+                            className="cursor-pointer justify-start gap-4 py-8 [&_svg]:size-8"
+                            onClick={() => handleSelect(id)}
+                            asChild
+                        >
+                            <div>
+                                <MdLocationOn />
+                                <div className="flex flex-col">
+                                    <div className="text-lg font-bold capitalize">
+                                        {nama.toLowerCase().replace("kab.", "kabupaten")}
+                                    </div>
+                                    <div className="capitalize text-gray-500">
+                                        {provinsi.nama.toLowerCase()}, Indonesia
+                                    </div>
+                                </div>
+                            </div>
+                        </Button>
+                    ))}
+                </div>
                 <DrawerFooter className="pt-2">
                     <DrawerClose asChild>
                         <Button variant="secondary">Cancel</Button>
