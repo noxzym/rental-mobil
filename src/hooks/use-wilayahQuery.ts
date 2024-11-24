2cdf937fed3588a2a6caa133c2ce0a63a9e52931
noxzym <orchitiadi@gmail.com>
2024-11-24 17:49:59 +0700

    chore(useWilayahQuery): simplify type

--- src/hooks/use-wilayahQuery.ts
+++ src/hooks/use-wilayahQuery.ts
@@ -1,19 +1,13 @@
 import { Prisma } from "@prisma/client";
 import { createQuery } from "floppy-disk";
 
-interface prop {
-    inputDebouncedValue?: string;
-}
+type props = { inputDebouncedValue?: string };
+type returnType = Prisma.kabukotaGetPayload<{ include: { provinsi: true } }>[];
 
-export function useWilayahQuery({ inputDebouncedValue }: prop) {
-    return createQuery<
-        { inputDebouncedValue?: string },
-        Prisma.kabukotaGetPayload<{ include: { provinsi: true } }>[]
-    >(async () => {
-        const res = await fetch(
-            `/api/wilayah${inputDebouncedValue?.length ? `?query=${inputDebouncedValue}` : ""}`
-        );
-        if (!res.ok) throw new Error("Failed to fetch data");
-        return res.json();
-    })({ inputDebouncedValue });
-}
+export const useWilayahQuery = createQuery<props, returnType>(async ({ inputDebouncedValue }) => {
+    const res = await fetch(
+        `/api/wilayah${inputDebouncedValue?.length ? `?query=${inputDebouncedValue}` : ""}`
+    );
+    if (!res.ok) throw new Error("Failed to fetch data");
+    return res.json();
+});
