35fef7bf9859ea4c6b518517aa15ef1ceebf7776
noxzym <orchitiadi@gmail.com>
2024-12-09 11:23:35 +0700

    chore: merge rename changes

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,7 +1,7 @@
 import { Viewport } from "next";
 import { Inter } from "next/font/google";
 import { Toaster } from "@/components/ui/toaster";
-import AuthProvider from "@/components/AuthProvider";
+import SessionProvider from "@/components/SessionProvider";
 import "./globals.css";
 
 const inter = Inter({ subsets: ["latin"] });
@@ -14,12 +14,12 @@ export default function RootLayout({
     return (
         <html lang="en" className="scroll-smooth">
             <body className={inter.className}>
-                <AuthProvider>
+                <SessionProvider>
                     <main className="mx-auto grid min-h-dvh grid-rows-[auto,1fr,auto]">
                         {children}
                     </main>
                     <Toaster />
-                </AuthProvider>
+                </SessionProvider>
             </body>
         </html>
     );
