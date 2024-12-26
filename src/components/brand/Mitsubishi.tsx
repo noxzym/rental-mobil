b64e1f63439281d35164d8631cdb5e2f806a4d8b
noxzym <orchitiadi@gmail.com>
2024-12-26 13:09:36 +0700

    chore(components): add brand logo

---
+++ src/components/brand/Mitsubishi.tsx
@@ -0,0 +1,12 @@
+// icon:mitsubishi | Simple Icons https://simpleicons.org | SimpleIcons.org
+import * as React from "react";
+
+function IconMitsubishi(props: React.SVGProps<SVGSVGElement>) {
+    return (
+        <svg viewBox="0 0 24 24" fill="currentColor" height="1em" width="1em" {...props}>
+            <path d="M8 22.38H0l4-6.92h8zm8 0h8l-4-6.92h-8zm0-13.84l-4-6.92-4 6.92 4 6.92z" />
+        </svg>
+    );
+}
+
+export default IconMitsubishi;
