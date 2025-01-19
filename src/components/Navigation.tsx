e2b9d4376c4642e4d021d4dabac585069d13b301
noxzym <orchitiadi@gmail.com>
2025-01-19 20:53:30 +0700

    menambahkan tombol navigasi

--- src/components/Navigation.tsx
+++ src/components/Navigation.tsx
@@ -6,11 +6,17 @@ import { Button } from "./ui/button";
 
 export default function Navigation() {
     return (
-        <nav className="container sticky top-0 z-10 flex items-center justify-between bg-background px-20 py-5">
+        <nav className="container sticky top-0 z-20 flex items-center justify-between bg-background px-20 py-5">
             <Link href="/" className="[&_svg]:h-auto [&_svg]:w-20">
                 <Logo />
             </Link>
             <div className="flex items-center gap-5">
+                <Button variant="link" className="px-0 py-0" asChild>
+                    <Link href="/">Halaman Utama</Link>
+                </Button>
+                <Button variant="link" className="px-0 py-0" asChild>
+                    <Link href="/search">Cari Mobil</Link>
+                </Button>
                 <Button variant="link" className="px-0 py-0" asChild>
                     <Link href="/dashboard">Buka Dashboard</Link>
                 </Button>
