e9a9d09ad5a8ade0873705d66ece56662881c966
noxzym <orchitiadi@gmail.com>
2025-01-19 19:59:35 +0700

    menambahkan driver kedalam query store hook

--- src/hooks/floppy-disk/use-queryStore.ts
+++ src/hooks/floppy-disk/use-queryStore.ts
@@ -5,11 +5,13 @@ type queryStore = {
     duration: string;
     location: string | null;
     time: string;
+    driver: boolean;
 };
 
 export const useQueryStore = createStore<queryStore>(({ set }) => ({
-    date: Date.now().toString(),
+    date: new Date(new Date().setHours(0, 0, 0, 0)).getTime().toString(),
     duration: "1",
     location: "3276",
-    time: ""
+    time: "",
+    driver: false
 }));
