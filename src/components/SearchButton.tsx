7566d15f7665660738976c486ba1b1162eefe71a
noxzym <orchitiadi@gmail.com>
2025-01-19 20:01:52 +0700

    menambahkan driver kedalam query sekaligus menghapus support untuk halaman search

--- src/components/SearchButton.tsx
+++ src/components/SearchButton.tsx
@@ -1,33 +1,23 @@
 "use client";
 
 import Link from "next/link";
-import { usePathname } from "next/navigation";
 import { useQueryStore } from "@/hooks/floppy-disk/use-queryStore";
 import { Button } from "@/components/ui/button";
 
 export default function SearchButton() {
-    const pathName = usePathname();
-    const { date, duration, location, time } = useQueryStore();
-
-    const isHomePage = pathName === "/";
+    const { date, duration, location, time, driver } = useQueryStore();
 
     return (
-        <Button
-            className={
-                isHomePage
-                    ? "w-full bg-[#1877F2] text-background hover:bg-[rgba(11,95,204)]"
-                    : "h-fit w-fit rounded-lg bg-[rgba(11,95,204,.1)] py-1 text-[#1877F2] hover:bg-[rgba(11,95,204,.2)]"
-            }
-            asChild
-        >
+        <Button className="w-full bg-[#1877F2] text-background hover:bg-[rgba(11,95,204)]" asChild>
             <Link
                 href={{
                     pathname: "/search",
                     query: {
-                        date: date,
-                        duration: duration,
-                        location: location,
-                        time: time
+                        date,
+                        duration,
+                        location,
+                        time,
+                        driver
                     }
                 }}
             >
